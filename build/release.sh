3196a4652148e165d7ed31d9720b0e12d48d8c09
Lizhong <lizhong.zhang@nist.gov>
2016-10-11 16:49:09 -0400

    Create a script to build a realse tar ball

---
+++ build/release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BUILD_PATH="$PWD"
+#BUILD_PATH="/tmp/dmoni/build"
+
+REPO_PATH="github.com/lizhongz/dmoni"
+BIN=dmoni
+SNAPSHOT=snapshot
+README=README.md
+
+# Check if temperary build dir exists
+if [ ! -d "$BUILD_PATH" ]; then
+    mkdir -p $BUILD_PATH
+fi
+
+cd $BUILD_PATH
+
+# Build DMoni
+go build -o $BIN $REPO_PATH 
+
+# Copy snapshot repo
+git clone --depth=1 git@gadget.ncsl.nist.gov:lizhong/snapshot.git $SNAPSHOT >/dev/null && rm -rf $SNAPSHOT/.git
+
+# Copy README
+cp $GOPATH/src/$REPO_PATH/README.md $README
+
+# Create a tarball
+tar --transform 's,^,dmoni/,' -zcvf $BIN.tar.gz $BIN $SNAPSHOT $README >/dev/null
+ 
+# Clean up
+rm -rf $BIN $SNAPSHOT $README
+
+cd - >/dev/null
