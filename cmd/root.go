0e05838db9520eb8a687cb34fad7989b16dd3b93
Lizhong <lizhong.zhang@nist.gov>
2017-01-31 15:47:20 -0500

    Create scripts to facilitate starting/stopping manager and agent daemon

--- cmd/root.go
+++ cmd/root.go
@@ -59,7 +59,7 @@ func init() {
 	// Cobra supports Persistent Flags, which, if defined here,
 	// will be global for your application.
 
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dmoni.yaml)")
+	//RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dmoni.yaml)")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	//RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
