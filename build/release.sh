0b211d30470c2ac7c18332f942fa0cf429641f6b
lizhong <lizhong.zhang@nist.gov>
2017-07-11 20:31:00 +0000

    Refactor the release.sh script

--- build/release.sh
+++ build/release.sh
@@ -4,7 +4,7 @@ VERSION="0.9.0"
 
 TMP_DIR="$PWD/dmoni-$VERSION"
 REPO="github.com/usnistgov/DMoni"
-SRC_DIR="$GOPATH/src/$REPO"
+SRC_DIR="$(go env GOPATH)/src/$REPO"
 
 # Check if build dir exists
 if [ ! -d "$TMP_DIR" ]; then
@@ -15,9 +15,11 @@ fi
 go build -o $TMP_DIR/bin/dmoni $REPO
 
 # Copy snapshot repo
-git clone --depth=1 git@gitlab.ncsl.nist.gov:lnz5/snapshot.git \
-    $TMP_DIR/snapshot >/dev/null && rm -rf $TMP_DIR/snapshot/.git
+#git clone --depth=1 git@gitlab.ncsl.nist.gov:lnz5/snapshot.git \
+#    $TMP_DIR/snapshot >/dev/null && rm -rf $TMP_DIR/snapshot/.git
 
+cp -r $SRC_DIR/snapshot $TMP_DIR
+cp -r $SRC_DIR/docs $TMP_DIR
 cp $SRC_DIR/README.md $TMP_DIR
 cp $SRC_DIR/tools/dmoni_manager.sh $SRC_DIR/tools/dmoni_agent.sh $TMP_DIR/bin
 
